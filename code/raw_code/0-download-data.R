8e407f9d47d62022411820cc7481390ec4a773e4
jtleek <jtleek@gmail.com>
2018-07-13 23:20:51 +0000

    Added the save

--- code/raw_code/0-download-data.R
+++ code/raw_code/0-download-data.R
@@ -6,3 +6,9 @@ txt = readtext(here("data/raw_data/posts/*"))
 # Looked at it
 library(dplyr)
 glimpse(txt)
+
+# Save as rda file
+data_date = date()
+save(txt,data_date,file=here("data/tidy_data",
+                             "simply_data_before_processing_07-13-2018.rda"))
+
